aa467bb17082e4f7bc169eeba33fd24328595757
David <2811641164@qq.com>
2024-09-02 19:38:01 +0800

    employee sign up

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,22 +1,23 @@
 package com.sky.controller.admin;
 
+import com.github.pagehelper.Page;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -79,13 +80,22 @@ public class EmployeeController {
 
     /**
      * employee sign up
+     *
      * @param employeeDTO
      * @return
      */
     @PostMapping
-    public Result employeeSignUp(@RequestBody EmployeeDTO employeeDTO){
+    public Result employeeSignUp(@RequestBody EmployeeDTO employeeDTO) {
         log.info("员工登录:{}", employeeDTO);
         employeeService.employeeSignUp(employeeDTO);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("employee page query")
+    public Result<PageResult> employeePageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("employee page query, parameter: {}", employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return null;
     }
 }
